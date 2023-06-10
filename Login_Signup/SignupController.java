b87a01eae5e569fe3487c7e165bc338b5d7011f1
Lark Sigmuond A. Babao <129244163+Snorlark@users.noreply.github.com>
2023-06-10 19:21:46 +0800

    Update SignupController.java

--- Login_Signup/SignupController.java
+++ Login_Signup/SignupController.java
@@ -2,12 +2,14 @@ package Login_Signup;
 
 import java.io.IOException;
 
+import Alert.AlertMaker;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
 import javafx.scene.input.MouseEvent;
@@ -21,23 +23,53 @@ public class SignupController{
     
     @FXML
     private PasswordField passwordField;
+
+    @FXML
+    private Label showPasswordLabel;
     
 
-    public void signup (ActionEvent event) throws IOException {
 
+    public void signup (ActionEvent event) throws IOException {    
+        
         String username = usernameField.getText();
         String password = passwordField.getText();
-
-        LoginController.username = username;
+        
+        LoginController.username = username; 
         LoginController.password = password;
 
-        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
-        FXMLLoader loader = new FXMLLoader(getClass().getResource("/Login_Signup/Login.fxml"));
-        Parent root = loader.load();
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-       
+        if (username.isEmpty() || password.isEmpty()) {
+
+            AlertMaker.showErrorAlert("Sign Up", "Fill out the important fields");
+            
+        }
+        
+        else {
+        
+            AlertMaker.showSuccessfulAlert("Sign Up", "You have been succesfully signed in");
+        
+            Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/Login_Signup/Login.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+            stage.setScene(scene);
+            stage.show();
+            
+        }
+                
+    }
+
+    public void showPassword (MouseEvent event) throws IOException {
+        
+        String pass = passwordField.getText();
+        showPasswordLabel.setText(pass);
+        showPasswordLabel.setLayoutY(229);       
+
+    }
+
+    public void hidePassword (MouseEvent event) throws IOException {
+        
+        showPasswordLabel.setLayoutY(413);       
+
     }
 
     public void goToLogin(MouseEvent event) throws IOException {
@@ -49,13 +81,5 @@ public class SignupController{
         stage.setScene(scene);
         stage.show();
     }
-
-    
-
-    
-    
-
-
-
 
 }
