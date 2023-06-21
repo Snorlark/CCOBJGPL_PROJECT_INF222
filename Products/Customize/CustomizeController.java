b0f22b2f255f1807d9b75b8e517cdefa5d641a16
Sherlene Agustin <129355802+shoreline0-0@users.noreply.github.com>
2023-06-22 07:37:53 +0800

    Add files via upload

--- Products/Customize/CustomizeController.java
+++ Products/Customize/CustomizeController.java
@@ -10,72 +10,147 @@ import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.scene.shape.Rectangle;
 import javafx.stage.Stage;
 
 public class CustomizeController{
 
     @FXML //customization buttons
-    private Button btnskin, btneye, btnmouth, btnhair, btn_outfit;
+    private Button btnskin, btneye, btnbrow, btnmouth, btnhair, btn_outfit;
     
     @FXML //selection panes
-    private Pane selectionhair;
+    private Pane 
+    base, selectionColor, 
+    selectionSkin, selectionEye, selectionBrow, selectionMouth, selectionHair, selectionOutfit,
+    skinGrp;
 
-    @FXML //image selections
-    private Button hair1a,hair1b,hair2a,hair2b;
+    // @FXML //color
+    private ToggleButton black, brown;
 
-    @FXML //selection panes
-    private Pane disphair1a, disphair1b, disphair2a, disphair2b;
+    // @FXML //image selections
+    private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8;
 
-    // @FXML
-    // private ImageView openEyes;
-
-    // @FXML
-    // private Rectangle openRectangle;
-
-    // @FXML
-    // private Label usernameLabel;
-
+    @FXML //selection and display imgs
+    private ImageView 
+    editedRoid,
+    skin, eyes, brow, mouth, hair, outfit,
+    skin1, skin2, skin3, skin4,
+    hair1a, hair1b, hair2a, hair2b,
+    toggle1_hair, toggle2_hair;
+    
 
     public void login (ActionEvent event) throws IOException {
-
+        //ToggleButton sourceToggleButton = (ToggleButton) event.getSource();
         Button sourceButton = (Button) event.getSource();
-        
-        if (sourceButton == btnhair) {
-            selectionhair.setVisible(true);
-            if (sourceButton == hair1a) { 
-                (disphair1a).setVisible(true);
-                (disphair1b).setVisible(false);
-                (disphair2a).setVisible(false);
-                (disphair2b).setVisible(false);
-            } else if (sourceButton == hair1a) {
-                (disphair1a).setVisible(false);
-                (disphair1b).setVisible(true);
-                (disphair2a).setVisible(false);
-                (disphair2b).setVisible(false);
-            } else if (sourceButton == hair2a) { 
-                (disphair1a).setVisible(false);
-                (disphair1b).setVisible(false);
-                (disphair2a).setVisible(true);
-                (disphair2b).setVisible(false);
-            } else if (sourceButton == hair2b) { 
-                (disphair1b).setVisible(true);
-                (disphair2a).setVisible(false);
-                (disphair2b).setVisible(false);
-            }
-        }
+        base.setVisible(true);
+        skinGrp.setVisible(false);
 
-        
-       
+        if (sourceButton == btnskin) {
+            selectionSkin.setVisible(true);
+            selectionColor.setVisible(false);
+            btn1.setVisible(true);
+            btn2.setVisible(true);
+            btn3.setVisible(true);
+            btn4.setVisible(true);
+
+            skinGrp.setVisible(true);
+            skin1.setVisible(true);
+
+            if (event.getSource() == btn1) { 
+                skin1.setVisible(true);
+            } else if (event.getSource() == btn2) {
+                skin2.setVisible(true);
+            } else if (event.getSource() == btn3) {
+                skin3.setVisible(true);
+            } else if (event.getSource() == btn4) { 
+                skin4.setVisible(true);
+            }
+            
+        // } else if (event.getSource() == btneye) {
+        //     selectionHair.setVisible(true);
+        //     selectionColor.setVisible(true);
+        //     toggle1_hair.setVisible(true);
+        //     toggle2_hair.setVisible(true);
+
+        //     if (sourceButton == btn1 && sourceButton == black) { 
+        //         hair.setImage(hair1a);
+        //     } else if (sourceButton == btn1 && sourceButton == brown) {
+        //             hair.setImage(hair1b);
+        //     } else if (sourceButton == btn2 && sourceButton == black) {
+        //             hair.setImage(hair2a);
+        //     } else if (sourceButton == btn2 && sourceButton == brown) { 
+        //             hair.setImage(hair2b);
+        //     }
+        // } else if (event.getSource() == btnbrow) {
+        //     selectionhair.setVisible(true);
+        //     toggle1_hair.setVisible(true);
+        //     toggle2_hair.setVisible(true);
+            
+        //     if (event.getSource() == btn1 && event.getSource() == black) { 
+        //         hair.setImage(hair1a);
+        //     } else if (event.getSource() == btn1 && event.getSource() == brown) {
+        //             hair.setImage(hair1b);
+        //     } else if (event.getSource() == btn2 && event.getSource() == black) {
+        //         hair.setImage(hair2a);
+        //     } else if (event.getSource() == btn2 && event.getSource() == brown) { 
+        //             hair.setImage(hair2b);
+        //     }
+        // } else if (event.getSource() == btnmouth) {
+        //     selectionhair.setVisible(true);
+        //     toggle1_hair.setVisible(true);
+        //     toggle2_hair.setVisible(true);
+
+        //     if (event.getSource() == btn1 && event.getSource() == black) { 
+        //         hair.setImage(hair1a);
+        //     } else if (event.getSource() == btn1 && event.getSource() == brown) {
+        //             hair.setImage(hair1b);
+        //     } else if (event.getSource() == btn2 && event.getSource() == black) {
+        //         hair.setImage(hair2a);
+        //     } else if (event.getSource() == btn2 && event.getSource() == brown) { 
+        //             hair.setImage(hair2b);
+        //     }
+        // } else if (event.getSource() == btnhair) {
+        //     selectionhair.setVisible(true);
+        //     toggle1_hair.setVisible(true);
+        //     toggle2_hair.setVisible(true);
+            
+        //     if (event.getSource() == btn1 && event.getSource() == black) { 
+        //         hair.setImage(hair1a);
+        //     } else if (event.getSource() == btn1 && event.getSource() == brown) {
+        //             hair.setImage(hair1b);
+        //     } else if (event.getSource() == btn2 && event.getSource() == black) {
+        //         hair.setImage(hair2a);
+        //     } else if (event.getSource() == btn2 && event.getSource() == brown) { 
+        //             hair.setImage(hair2b);
+        //     }
+        // } else if (event.getSource() == btn_outfit) {
+        //     selectionhair.setVisible(true);
+        //     toggle1_hair.setVisible(true);
+        //     toggle2_hair.setVisible(true);
+
+        //     if (event.getSource() == btn1 && event.getSource() == black) { 
+        //         hair.setImage(hair1a);
+        //     } else if (event.getSource() == btn1 && event.getSource() == brown) {
+        //             hair.setImage(hair1b);
+        //     } else if (event.getSource() == btn2 && event.getSource() == black) {
+        //         hair.setImage(hair2a);
+        //     } else if (event.getSource() == btn2 && event.getSource() == brown) { 
+        //             hair.setImage(hair2b);
+        //     }
+        }
     }
-    
-    
-    
+
 }
+
 
